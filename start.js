49d72f645375e101f2a5e5aee0618a72efd5719d
SunghoonKim-UA <70456179+SunghoonKim-UA@users.noreply.github.com>
2021-03-25 21:39:56 -0700

    to apply google cloud

--- start.js
+++ start.js
@@ -18,21 +18,22 @@ require('./models/glider');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-const httpserver = app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
 
 var https = require('https');
-var key = fs.readFileSync('./selfsigned.key');
-var cert = fs.readFileSync('./selfsigned.crt');
-
-var credentials = {
-  key: key,
-  cert: cert
-};
-
-var httpsPort = 8443;
-const httpsServer = https.createServer(credentials, app)
-                         .listen(httpsPort, () => {
-  console.log("Https server listing on port : " + httpsPort)
-});
+const fs = require('fs');
+// var key = fs.readFileSync('./selfsigned.key');
+// var cert = fs.readFileSync('./selfsigned.crt');
+//
+// var credentials = {
+//   key: key,
+//   cert: cert
+// };
+//
+// var httpsPort = 8443;
+// const httpsServer = https.createServer(credentials, app)
+//                          .listen(httpsPort, () => {
+//   console.log("Https server listing on port : " + httpsPort)
+// });
